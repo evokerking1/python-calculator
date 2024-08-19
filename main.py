8e18971f94f55deed46dd336ebf2aa2261c71254
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-08-19 18:16:51 -0400

    Added exit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+
 print("Hello! Welcome to evokerking's calculator program!"
       "\nThis program is a simple calculator that can perform Addition, Subtraction, Multiplication and Division.")
 print("Please enter the numbers you want to perform operations on.")
@@ -8,15 +11,24 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Exit")
 operation = int(input("Enter the operation number: "))
 if operation == 1:
     print("The result of addition is: ", num1 + num2)
 elif operation == 2:
     print("The result of subtraction is: ", num1 - num2)
 elif operation == 3:
-    print("The result of multiplication is: ", num1 * num2)
+    suboperation = int(input("Enter the sub-operation number: "))
+    if suboperation == 1:
+        print("The result of multiplication is: ", num1 * num2)
+    elif suboperation == 2:
+        print("The result of division is: ", num1 / num2)
+    else:
+        print("Invalid sub-operation number. Please enter a valid sub-operation number.")
 elif operation == 4:
     print("The result of division is: ", num1 / num2)
+elif operation == 5:
+    _exit = os.system("exit")
 else:
     print("Invalid operation number. Please enter a valid operation number.")
 print("Thank you for using my calculator program!")
