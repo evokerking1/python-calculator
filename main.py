b122aa2f6459248374897833e61e929d274cca81
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-08-20 14:22:44 -0400

    Finished implementing exponents, fixed a bug with my previous update and fixed, it was sub-operations not displaying the list when selected

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+def exit():
+    _exit = os.system("exit")
 
 print("Hello! Welcome to evokerking's calculator program!"
       "\nThis program is a simple calculator that can perform Addition, Subtraction, Multiplication and Division.")
@@ -18,16 +20,16 @@ if operation == 1:
 elif operation == 2:
     print("The result of subtraction is: ", num1 - num2)
 elif operation == 3:
-    suboperation = int(input("Enter the sub-operation number: "))
     print("1. Multiplication")
     print("2. exponents")
     print("3. Exit")
+    suboperation = int(input("Enter the sub-operation number: "))
     if suboperation == 1:
         print("The result of multiplication is: ", num1 * num2)
     elif suboperation == 2:
         print("The result of division is: ", num1 ** num2)
     elif suboperation == 3:
-        _exit = os.system("exit")
+        exit()
     else:
         print("Invalid sub-operation number. Please enter a valid sub-operation number.")
 elif operation == 4:
@@ -43,11 +45,13 @@ elif operation == 4:
     elif suboperation == 3:
         print("The result of division is: ", num1 // num2)
     elif suboperation == 4:
-        _exit = os.system("exit")
+        exit()
     else:
         print("Invalid sub-operation number. Please enter a valid sub-operation number.")
 elif operation == 5:
-    _exit = os.system("exit")
+    exit()
 else:
     print("Invalid operation number. Please enter a valid operation number.")
 print("Thank you for using my calculator program!")
+
+print("Please join my discord server: https://discord.gg/ for updates on all my projects, for a list of my projects, go to https://evokerking.dev/")
